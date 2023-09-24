98499e5407d9228fb0efaa70c5b1f79458a30b34
KoiCrystal <jinyuyuyu@tutanota.com>
2023-09-24 14:32:39 +0800

    Update SysOperLog.java

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SysOperLog.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysOperLog.java
@@ -23,7 +23,7 @@ public class SysOperLog extends BaseEntity
     @Excel(name = "操作模块")
     private String title;
 
-    /** 业务类型（0其它 1新增 2修改 3删除） */
+    /** 业务类型（0其它 1新增 2修改 3删除 4授权 5导出 6导入 7强退 8生成代码 9清空数据） */
     @Excel(name = "业务类型", readConverterExp = "0=其它,1=新增,2=修改,3=删除,4=授权,5=导出,6=导入,7=强退,8=生成代码,9=清空数据")
     private Integer businessType;
 
